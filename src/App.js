3d9ea1fc3d3cfb5dedaa2ece9cd66026a475b674
anhdotdo <101596722+anhdotdo@users.noreply.github.com>
2022-08-02 01:48:33 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello, World!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,12 +19,10 @@ function App() {
         >
           Learn React
         </a>
-	<div>
-	  <h1> Hello World !!!</h1>
-	</div>
       </header>
     </div>
   );
 }
 
 export default App;
+
