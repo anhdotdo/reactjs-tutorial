c65f298b9f194c6b7ac71d255a476e6c7e156559
anhdotdo <anhdo157hcl@gmail.com>
2022-08-01 18:09:55 +0000

    MODIFY msg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <h1>Hello, World!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+	<div>
+	  <h1> Hello World !!!</h1>
+	</div>
       </header>
     </div>
   );
