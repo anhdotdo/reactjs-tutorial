bb5364bb43896d73b2dc364677336ea1067bab3d
anhdotdo <anhdo157hcl@gmail.com>
2022-08-01 18:36:35 +0000

    MODIFY App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         
-        <h1>Hello, World!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
